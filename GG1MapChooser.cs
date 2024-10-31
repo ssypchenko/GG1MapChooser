240b92715ab441abb954a1a5c254d7fbd24b75ef
ssypchenko <30598453+ssypchenko@users.noreply.github.com>
2024-10-31 16:35:54 +0000

    Version number

--- GG1MapChooser.cs
+++ GG1MapChooser.cs
@@ -31,7 +31,7 @@ namespace MapChooser;
 public class MapChooser : BasePlugin, IPluginConfig<MCConfig>
 {
     public override string ModuleName => "GG1_MapChooser";
-    public override string ModuleVersion => "v1.5.0";
+    public override string ModuleVersion => "v1.5.1";
     public override string ModuleAuthor => "Sergey";
     public override string ModuleDescription => "Map chooser, voting, rtv, nominate, etc.";
     public MCCoreAPI MCCoreAPI { get; set; } = null!;
